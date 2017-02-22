97ce4524ea12942e65640a5c751097652bfaa0fb
mac <ghita.najibi@gmail.com>
2017-02-23 00:48:57 +0100

    gh

---
+++ Partie 1.py
@@ -0,0 +1,11 @@
+class Bond(object):
+    def _self_(self, stbond, ltbond):
+        self.stbond = stbond
+        self.ltbond = ltbond
+
+class bond(object)
+    def _self_(self, term, amount, rate):
+        self.term = term
+        self.amount = amount
+        self.rate = rate
+
